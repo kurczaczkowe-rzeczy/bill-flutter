d7685071475f440c11e92a01a8e8643d04280784
RowickaM <rowickam.23@gmail.com>
2023-02-07 19:36:04 +0100

    #14 toggle product to in/out cart

--- lib/screen/shoppingList/shopping_list_item.dart
+++ lib/screen/shoppingList/shopping_list_item.dart
@@ -5,16 +5,33 @@ class ShoppingListItemWidget extends StatelessWidget {
   final ShoppingItem item;
   final ValueSetter<int> onPressed;
 
-  const ShoppingListItemWidget({Key? key, required this.item, required this.onPressed}) : super(key: key);
+  const ShoppingListItemWidget(
+      {Key? key, required this.item, required this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-          vertical: 16.0, horizontal: 30.0),
-      child: Text(
-        item.name,
-        style: TextStyle(fontWeight: FontWeight.w500),
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+      child: GestureDetector(
+        onTap: () => onPressed(item.id),
+        child: Row(
+          children: [
+            Checkbox(
+              value: item.inCart,
+              onChanged: (_) => onPressed(item.id),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4.0))),
+            ),
+            Text(
+              item.name,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: item.inCart ? Colors.black12 : Colors.black,
+              decoration: item.inCart ? TextDecoration.lineThrough : TextDecoration.none),
+            ),
+          ],
+        ),
       ),
     );
   }
